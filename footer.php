189ffdc63b0e51a3a122bf8a55768fe93d5c3070
Walembocoumbou <139353657+Walembocoumbou@users.noreply.github.com>
2023-11-15 10:36:52 +0100

    Poursuite 01 modification du footer

--- footer.php
+++ footer.php
@@ -1,11 +1,27 @@
 <footer class="hg_footer">
 
     <vision class="hg_vision">
-        <article class="box">
-        Article 1
+        <article class="site-footer">
+            <ul>
+                <li><a href="http://www.vatican.va/phome_fr.htm">LE SAINT-SIÈGE</a></li>
+                <li><a href="http://biayenda.net">LA CAUSE DU CARDINAL ÉMILE BIAYENDA</a></li>
+                <li><a href="http://acerac.org">ASSOCIATION DES CONFÉRENCES ÉPISCOPALES<br /> DE L'AFRIQUE CENTRALE</a></li>
+                <li><a href="http://www.archidiocesedebrazzaville.org">ARCHIDIOCÈSE DE BRAZZAVILLE</a></li>
+                <li><a href="http://eveche-impfondo.eklablog.com">DIOCÈSE D'IMPFONDO</a></li>
+                <li><a href="http://www.diocesededolisie.org">DIOCÈSE DE DOLISIE</a></li>
+                <li><a href="http://diocesedekinkala.free.fr">DIOCÈSE DE KINKALA</a></li>
+            </ul>
         </article>
-        <article class="box">
-        Article 2
+        <article class="site-footer">
+            <ul>
+                <li><a href="http://www.diocese-pointe-noire-congo.org/">DIOCÈSE DE POINTE-NOIRE</a></li>
+                <li><a href="http://www.dioceseowando.org/">DIOCÈSE D'OWANDO</a></li>
+                <li><a href="http://www.lasemaineafricaine.net">LA SEMAINE AFRICAINE</a></li>
+                <li><a href="http://www.croire.com">FOI ET QUESTIONS CHRÉTIENNES</a></li>
+                <li><a href="http://www.Catholique.org/">PORTAIL CATHOLIQUE FRANCOPHONE</a></li>
+                <li><a href="http://jesus.catholique.fr/">RENCONTRER JESUS</a></li>
+                <li><a href="http://info.catho.be">AGENCE D'INFORMATIONS RELIGIEUSES</a></li>
+            </ul>
         </article>
     </vision>
 
